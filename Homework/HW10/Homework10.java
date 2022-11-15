cb136c95f60feda65d4a32b8e5e93239ef677786
dinger16 <72777253+dinger16@users.noreply.github.com>
2022-11-15 10:15:48 -0500

    update

--- Homework/HW10/Homework10.java
+++ Homework/HW10/Homework10.java
@@ -1,5 +1,5 @@
 public class Homework10 {
-  public static boolean check(char[] correctPositions, String word) {
+  public boolean check(char[] correctPositions, String word) {
     for (int i = 0; i < correctPositions.length; i++) {
       if (correctPositions[i] != '*' && correctPositions[i] != word.charAt(i))
         return false;
@@ -8,10 +8,26 @@ public class Homework10 {
   }
 
   public static void main(String[] args) {
-    // char[] correctPositions = new char[]{'W', '*', 'R'};
-    // String word="WoRdle";
-    char[] correctPositions = new char[]{ 'w', '*' };
-    String word = "wordle";
-    System.out.println(check(correctPositions, word));
+    Homework10 m = new Homework10();
+
+    char[] correctPositions = new char[]{ 'W', '*', 'R'};
+    String word = "WoRdle";
+    System.out.println(m.check(correctPositions, word));
+
+    char[] correctPositions1 = new char[]{ 'W', '*' };
+    String word1 = "wordle";
+    System.out.println(m.check(correctPositions1, word1));
+
+    char[] correctPositions2 = new char[]{ 'S', '*', 'G', '*', 'R'};
+    String word2 = "SUGAR";
+    System.out.println(m.check(correctPositions2, word2));
+
+    char[] correctPositions3 = new char[]{ }; 
+    String word3 = "";
+    System.out.println(m.check(correctPositions3, word3));
+
+    char[] correctPositions4 = new char[]{ '*', '*', '*', '*', '*'};
+    String word4 = "PARADIGMS";
+    System.out.println(m.check(correctPositions4, word4));
   }
 }
